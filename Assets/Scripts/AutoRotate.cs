20d3a3237a415095e8f810feaae14497c1f2f8ae
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-22 18:07:37 +0200

    added documentation to the AutoRotate script

--- Assets/Scripts/AutoRotate.cs
+++ Assets/Scripts/AutoRotate.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class AutoRotate : MonoBehaviour
 {
+    // Declaring and initializing the constant.
     private const float RotationSpeed = 65.0f;
     
     // Update is called once per frame.
     private void Update()
     {
+        // Executing the rotation around the y-axis.
         transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
     }
 }
