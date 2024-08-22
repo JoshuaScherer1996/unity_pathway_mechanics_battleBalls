3c0d346ffca2c04a6bc99212a0485472a9d30494
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-22 17:29:25 +0200

    implemented random spawn locations inside the SpawnManager script

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private const float SpawnRange = 9.0f;
     
     // Start is called before the first frame update.
     private void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        var spawnRangeX = Random.Range(-SpawnRange, SpawnRange);
+        var spawnRangeZ = Random.Range(-SpawnRange, SpawnRange);
+
+        var randomPos = new Vector3(spawnRangeX, 0, spawnRangeZ);
+        
+        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
     }
 }
